04f24bc7bba7e61616f33f27f15f95f1ec9cb6d8
Ali Guzelyel <aguzelyel@hawk.iit.edu>
2024-03-24 13:29:43 -0500

    decoder model finalized

--- v2.py
+++ v2.py
@@ -194,5 +194,5 @@ for iter in range(max_iters):
     optimizer.step()
 
 context = torch.zeros((1,1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
 
