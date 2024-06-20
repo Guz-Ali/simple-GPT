0bb55e72b1b37d4bd28c34cfe991598b978d294e
Ali Guzelyel <aguzelyel@hawk.iit.edu>
2024-06-20 16:07:40 -0500

    parameter sharing, weights

--- train_gpt2.py
+++ train_gpt2.py
@@ -84,6 +84,9 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
     def forward(self, idx, targets=None):
         B, T = idx.size()
         assert T <= self.config.block_size, f"cannot forward seq of len {T}"
